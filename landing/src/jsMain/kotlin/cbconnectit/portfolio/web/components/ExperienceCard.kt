1c3128cc3bbcf4323b5d4486b540d441348c77f5
ShaHar91 <bollachristiano@gmail.com>
2025-09-26 17:49:56 +0200

    feat: [#11] Refactored the ExperienceSection

--- landing/src/jsMain/kotlin/cbconnectit/portfolio/web/components/ExperienceCard.kt
+++ landing/src/jsMain/kotlin/cbconnectit/portfolio/web/components/ExperienceCard.kt
@@ -6,12 +6,9 @@ import cbconnectit.portfolio.web.backdropGradient
 import cbconnectit.portfolio.web.data.models.domain.Experience
 import cbconnectit.portfolio.web.extensions.techStackSvg
 import cbconnectit.portfolio.web.primaryGradient
-import com.materialdesignsystem.components.Spacer
+import com.materialdesignsystem.components.widgets.DsCard
 import com.materialdesignsystem.toColorScheme
-import com.varabyte.kobweb.compose.css.BoxShadow
-import com.varabyte.kobweb.compose.css.FontWeight
-import com.varabyte.kobweb.compose.css.TextAlign
-import com.varabyte.kobweb.compose.css.WhiteSpace
+import com.varabyte.kobweb.compose.css.*
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
@@ -20,195 +17,164 @@ import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.thenIf
-import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.breakpoint.displayIfAtLeast
+import com.varabyte.kobweb.silk.style.breakpoint.displayUntil
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import org.jetbrains.compose.web.css.DisplayStyle
-import org.jetbrains.compose.web.css.LineStyle
-import org.jetbrains.compose.web.css.percent
-import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.dom.P
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.css.*
+
+fun Modifier.gridTemplate(vararg sizes: CSSLengthOrPercentageNumericValue) = gridTemplateColumns {
+    sizes.forEach { size(it) }
+}
 
 @Composable
 fun ExperienceCard(
-    breakpoint: Breakpoint,
     active: Boolean = false,
     experience: Experience,
 ) {
     val baseModifier = Modifier.fillMaxWidth().display(DisplayStyle.Grid)
-    if (breakpoint > Breakpoint.MD) {
-        Row(
-            modifier = baseModifier
-                .gridTemplateColumns {
-                    size(20.percent)
-                    size(10.percent)
-                    size(70.percent)
-                },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            ExperienceDetails(breakpoint, experience)
-            ExperienceNumber(active, experience)
-            ExperienceDescription(breakpoint, active, experience.description)
-        }
-    } else {
-        Row(
-            modifier = baseModifier
-                .gridTemplateColumns {
-                    size(15.percent)
-                    size(85.percent)
-                },
-        ) {
-            ExperienceNumber(active, experience)
 
-            Column(
-                modifier = Modifier.margin(topBottom = 20.px),
-                verticalArrangement = Arrangement.spacedBy(8.px)
-            ) {
-                ExperienceDetails(breakpoint, experience)
-                ExperienceDescription(breakpoint, active, experience.description)
-            }
+    Row(
+        modifier = baseModifier
+            .displayIfAtLeast(Breakpoint.LG)
+            .gridTemplate(20.percent, 10.percent, 70.percent),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ExperienceDetails(experience)
+        TimelineMarker(active, experience)
+        ExperienceDescription(active, experience.description)
+    }
+
+    Row(
+        modifier = baseModifier
+            .displayUntil(Breakpoint.LG)
+            .gridTemplate(15.percent, 85.percent)
+    ) {
+        TimelineMarker(active, experience)
+
+        Column(
+            modifier = Modifier.margin(topBottom = 20.px),
+            verticalArrangement = Arrangement.spacedBy(8.px)
+        ) {
+            ExperienceDetails(experience)
+            ExperienceDescription(active, experience.description)
         }
     }
 }
 
+private val CardShadow = listOf(
+    BoxShadow.of(0.px, 1.px, 2.px, 0.px, Colors.Black.copyf(alpha = 0.3f)),
+    BoxShadow.of(0.px, 2.px, 6.px, 2.px, Colors.Black.copyf(alpha = 0.15f))
+)
+
 @Composable
 private fun ExperienceDescription(
-    breakpoint: Breakpoint,
     active: Boolean,
     description: String
 ) {
+    val breakpoint = rememberBreakpoint()
     val colorMode by ColorMode.currentState
 
-    val gradient = colorMode.backdropGradient
-    val activeGradient = colorMode.primaryGradient
+    val textColor = if (active) colorMode.toColorScheme.onPrimary else colorMode.toColorScheme.onSurface
+    val backgroundGradient = if (active) colorMode.primaryGradient else colorMode.backdropGradient
 
     // TODO: could be replaced with a DsCard in the future...
     //      but it requires some adjustments to the DsCard to be more flexible and have more styles
     Box(
         modifier = Modifier.fillMaxWidth()
-            .thenIf(breakpoint > Breakpoint.MD) {
-                Modifier.margin(topBottom = 20.px)
-            }
+            .margin(topBottom = if (breakpoint > Breakpoint.MD) 20.px else 0.px)
             .padding(all = 14.px)
-            .color(if (active) colorMode.toColorScheme.onPrimary else colorMode.toColorScheme.onSurface)
-            .backgroundImage(if (active) activeGradient else gradient)
+            .color(textColor)
+            .backgroundImage(backgroundGradient)
             .borderRadius(12.px)
-            .boxShadow(
-                listOf(
-                    BoxShadow.of(0.px, 1.px, 2.px, 0.px, Colors.Black.copyf(alpha = 0.3f)),
-                    BoxShadow.of(0.px, 2.px, 6.px, 2.px, Colors.Black.copyf(alpha = 0.15f))
-                )
-            )
+            .boxShadow(CardShadow)
     ) {
-        P(
-            attrs = Modifier
-                .margin(topBottom = 0.px)
-                .fontSize(16.px)
-                .whiteSpace(WhiteSpace.PreLine)
-                .fontWeight(FontWeight.Normal)
-                .lineHeight(1.65)
-                .toAttrs()
-        ) {
-            Text(description)
-        }
+        SpanText(
+            text = description,
+            modifier = Modifier.lineHeight(1.65)
+        )
     }
 }
 
 @Composable
 private fun ExperienceDetails(
-    breakpoint: Breakpoint,
-    experience: Experience,
+    experience: Experience
 ) {
-    Row(
-        modifier = Modifier.fillMaxWidth()
-            .fillMaxHeight(),
-        verticalAlignment = Alignment.CenterVertically
+    val breakpoint = rememberBreakpoint()
+    val textAlign = if (breakpoint > Breakpoint.MD) TextAlign.End else TextAlign.Start
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(left = 32.px),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = if (breakpoint > Breakpoint.MD) Alignment.End else Alignment.Start
     ) {
-        Spacer(Modifier.width(40.px))
-
-        Column(
+        SpanText(
+            text = experience.formattedJobPosition,
             modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = if (breakpoint > Breakpoint.MD) Alignment.End else Alignment.Start
-        ) {
-            P(
-                attrs = Modifier
-                    .margin(topBottom = 0.px)
-                    .fontSize(20.px)
-                    .fontWeight(FontWeight.Bold)
-                    .color(ColorMode.current.toColorScheme.primary)
-                    .thenIf(breakpoint > Breakpoint.MD) {
-                        Modifier.textAlign(TextAlign.End)
-                    }
-                    .toAttrs()
-            ) {
-                Text(experience.formattedJobPosition)
-            }
+                .fontSize(20.px)
+                .fontWeight(FontWeight.Bold)
+                .color(ColorMode.current.toColorScheme.primary)
+                .textAlign(textAlign)
+        )
 
-            P(
-                attrs = Modifier
-                    .margin(topBottom = 0.px)
-                    .fontSize(14.px)
-                    .fontWeight(FontWeight.Normal)
-                    .thenIf(breakpoint > Breakpoint.MD) {
-                        Modifier.textAlign(TextAlign.End)
-                    }
-                    .toAttrs()
-            ) {
-                Text(experience.formattedDate)
-            }
+        SpanText(
+            text = experience.formattedDate,
+            modifier = Modifier
+                .fontSize(14.px)
+                .textAlign(textAlign)
+        )
 
-            P(
-                attrs = Modifier
-                    .margin(topBottom = 0.px)
-                    .fontSize(14.px)
-                    .fontWeight(FontWeight.Normal)
-                    .thenIf(breakpoint > Breakpoint.MD) {
-                        Modifier.textAlign(TextAlign.End)
-                    }
-                    .toAttrs()
-            ) {
-                Text(experience.company.name)
-            }
-        }
+        SpanText(
+            text = experience.company.name,
+            modifier = Modifier
+                .fontSize(14.px)
+                .textAlign(textAlign)
+        )
     }
 }
 
 @Composable
-private fun ExperienceNumber(
+private fun TimelineMarker(
     active: Boolean,
     experience: Experience
 ) {
     val colorMode by ColorMode.currentState
 
-    Box(
+    Column(
         modifier = Modifier.fillMaxHeight(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // Vertical upper line
         Box(
-            modifier = Modifier.fillMaxHeight()
+            modifier = Modifier
+                .weight(1)
                 .width(3.px)
                 .backgroundColor(colorMode.toColorScheme.primary)
         )
 
-        Box(
+        Column(
             modifier = Modifier
                 .padding(leftRight = 4.px, topBottom = 10.px)
                 .border(width = 3.px, style = LineStyle.Solid, colorMode.toColorScheme.primary)
-                .backgroundColor(if (active) colorMode.toColorScheme.primary else colorMode.toColorScheme.background)
+                .backgroundColor(if (active) colorMode.toColorScheme.primary else Color.transparent)
                 .borderRadius(50.px),
-            contentAlignment = Alignment.Center
+            verticalArrangement = Arrangement.spacedBy(10.px)
         ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(10.px)
-            ) {
-                experience.tags.forEach { tag ->
-                    experience.techStackSvg(tag, if (active) colorMode.toColorScheme.onPrimary else colorMode.toColorScheme.primary)
-                }
+            experience.tags.forEach { tag ->
+                experience.techStackSvg(tag, if (active) colorMode.toColorScheme.onPrimary else colorMode.toColorScheme.primary)
             }
         }
+
+        // Vertical bottom line
+        Box(
+            modifier = Modifier
+                .weight(1)
+                .width(3.px)
+                .backgroundColor(colorMode.toColorScheme.primary)
+        )
     }
 }
--- landing/src/jsMain/kotlin/cbconnectit/portfolio/web/pages/sections/ExperienceSection.kt
+++ landing/src/jsMain/kotlin/cbconnectit/portfolio/web/pages/sections/ExperienceSection.kt
@@ -6,11 +6,14 @@ import cbconnectit.portfolio.web.components.SectionTitle
 import cbconnectit.portfolio.web.data.models.domain.Experience
 import cbconnectit.portfolio.web.navigation.Navigation
 import cbconnectit.portfolio.web.utils.Constants
-import com.materialdesignsystem.components.Spacer
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.id
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.scrollMargin
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import org.jetbrains.compose.web.css.percent
@@ -23,25 +26,22 @@ fun ExperienceSection(experiences: List<Experience>) {
     Column(
         modifier = Modifier
             .id(Navigation.Screen.Home.ExperienceSection.id)
-            .scrollMargin(80.px)
+            .scrollMargin(Constants.HEADER_HEIGHT.px)
             .fillMaxWidth(if (breakpoint >= Breakpoint.MD) 80.percent else 90.percent)
             .maxWidth(Constants.SECTION_WIDTH.px),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(25.px)
     ) {
         SectionTitle(
-            modifier = Modifier
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             section = Navigation.Screen.Home.ExperienceSection,
             alignment = Alignment.CenterHorizontally,
-            href = null // TODO: add navigation
-//            showSeeAllButton = true
+            href = null
         )
-
-        Spacer(Modifier.height(25.px))
 
         Column {
             experiences.forEachIndexed { index, experience ->
-                ExperienceCard(breakpoint, index == 0, experience)
+                ExperienceCard(index == 0, experience)
             }
         }
     }
