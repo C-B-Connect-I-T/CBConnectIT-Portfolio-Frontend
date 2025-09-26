e9a10025f6be5531e1ee2106e5514aae2327b91c
ShaHar91 <bollachristiano@gmail.com>
2025-09-26 16:14:01 +0200

    feat: [#11] Refactored the PortfolioSection

--- landing/src/jsMain/kotlin/cbconnectit/portfolio/web/pages/sections/PortfolioSection.kt
+++ landing/src/jsMain/kotlin/cbconnectit/portfolio/web/pages/sections/PortfolioSection.kt
@@ -13,6 +13,7 @@ import com.materialdesignsystem.components.widgets.DsBorderRadius
 import com.materialdesignsystem.components.widgets.TextButton
 import com.materialdesignsystem.toColorScheme
 import com.varabyte.kobweb.compose.css.*
+import com.varabyte.kobweb.compose.css.functions.linearGradient
 import com.varabyte.kobweb.compose.css.functions.url
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
@@ -21,13 +22,11 @@ import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.thenIf
-import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.forms.ButtonSize
 import com.varabyte.kobweb.silk.components.forms.ButtonVars
+import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.style.selectors.active
 import com.varabyte.kobweb.silk.style.selectors.focus
 import com.varabyte.kobweb.silk.style.selectors.hover
 import com.varabyte.kobweb.silk.style.toModifier
@@ -35,8 +34,9 @@ import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import kotlinx.browser.window
 import org.jetbrains.compose.web.css.*
-import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
+
+fun Background.Companion.of(color: CSSColorValue) = ", $color".unsafeCast<Background.Listable>()
 
 @Composable
 fun PortfolioSection(
@@ -44,101 +44,70 @@ fun PortfolioSection(
     selectedWork: Project?,
     onWorkSelected: (Project) -> Unit
 ) {
+    val overlayColor = ColorMode.current.toColorScheme.primary.toRgb().copy(alpha = 210)
     val breakpoint = rememberBreakpoint()
 
     Column(
         modifier = Modifier
             .id(Navigation.Screen.Home.PortfolioSection.id)
-            .scrollMargin(80.px)
+            .scrollMargin(Constants.HEADER_HEIGHT.px)
             .fillMaxWidth(if (breakpoint >= Breakpoint.MD) 80.percent else 90.percent)
             .maxWidth(Constants.SECTION_WIDTH.px),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         SectionTitle(
-            Modifier.fillMaxWidth().margin(bottom = 25.px),
-            Navigation.Screen.Home.PortfolioSection,
+            modifier = Modifier.fillMaxWidth().margin(bottom = 25.px),
+            section = Navigation.Screen.Home.PortfolioSection,
             showSeeAllButton = true,
             href = Navigation.Screen.Projects.route
         )
 
         Box(
             Modifier
-                .borderRadius(20.px)
-                .backgroundImage(url(selectedWork?.bannerImageUrl ?: Res.Image.portfolio1))
-                .background {
-                    size(BackgroundSize.Cover)
-                    position(BackgroundPosition.of(CSSPosition(50.percent, 50.percent)))
-                }
                 .fillMaxWidth()
+                .borderRadius(20.px)
+                .background(
+                    Background.of(
+                        image = BackgroundImage.of(url(selectedWork?.bannerImageUrl ?: Res.Image.portfolio1)),
+                        position = BackgroundPosition.of(CSSPosition(50.percent, 50.percent)),
+                        size = BackgroundSize.Cover
+                    ),
+                    Background.of(BackgroundImage.of(linearGradient(overlayColor, overlayColor)))
+                )
         ) {
-            Box(
-                Modifier
-                    .fillMaxSize()
-                    .borderRadius(20.px)
-                    .backgroundColor(ColorMode.current.toColorScheme.primary.toRgb().copy(alpha = 210))
-            )
 
             Column(
-                Modifier.fillMaxSize()
-                    .padding(
-                        leftRight = if (breakpoint >= Breakpoint.MD) 36.px else 16.px,
-                        topBottom = if (breakpoint >= Breakpoint.MD) 36.px else 16.px
-                    )
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(if (breakpoint >= Breakpoint.MD) 36.px else 16.px),
+                verticalArrangement = Arrangement.spacedBy(if (breakpoint >= Breakpoint.MD) 36.px else 24.px)
             ) {
                 Box(
-                    Modifier
+                    modifier = Modifier
                         .fillMaxWidth()
                         .display(DisplayStyle.Flex)
                         .flexDirection(FlexDirection.Row)
                         .flexWrap(FlexWrap.Wrap)
                         .gap(12.px)
-                        .margin(bottom = 8.px)
                 ) {
                     projects.forEach { project ->
-                        P(
-                            ProjectNameStyle.toModifier()
-                                .color(ColorMode.current.toColorScheme.onPrimary)
-                                .thenIf(project.id == selectedWork?.id) {
-                                    Modifier
-                                        .backgroundColor(ColorMode.current.toColorScheme.surface)
-                                        .color(ColorMode.current.toColorScheme.onSurface)
-                                }
-                                .onClick {
-                                    onWorkSelected(project)
-                                }
-                                .padding(topBottom = 4.px, leftRight = 10.px)
-                                .margin(topBottom = 0.px)
-                                .fontSize(14.px)
-                                .borderRadius(6.px)
-                                .cursor(Cursor.Pointer)
-                                .userSelect(UserSelect.None) // No selecting text within buttons
-                                .maxLines(1)
-                                .toAttrs()
-                        ) { Text(project.title) }
+                        SpanText(
+                            text = project.title,
+                            modifier = ProjectNameStyle.toModifier()
+                                .classNames(if (project.id == selectedWork?.id) "active" else "inactive")
+                                .onClick { onWorkSelected(project) }
+                        )
                     }
                 }
 
-                Spacer(Modifier.height(if (breakpoint >= Breakpoint.MD) 36.px else 24.px))
-
-                P(
-                    Modifier
-                        .fillMaxWidth(if (breakpoint > Breakpoint.MD) 65.percent else 100.percent)
-                        .margin(top = if (breakpoint > Breakpoint.MD) 100.px else 24.px, bottom = 0.px)
-                        .fontSize(18.px)
-                        .thenIf(breakpoint > Breakpoint.MD) { Modifier.fontSize(36.px) }
-                        .thenIf(breakpoint == Breakpoint.MD) { Modifier.fontSize(24.px) }
-                        .color(ColorMode.current.toColorScheme.onPrimary)
-                        .toAttrs()
-                ) {
-                    Text(selectedWork?.shortDescription ?: "")
-                }
-
-                Spacer(Modifier.height(if (breakpoint >= Breakpoint.MD) 36.px else 24.px))
+                SpanText(
+                    text = selectedWork?.shortDescription ?: "",
+                    modifier = ProjectDescriptionStyle.toModifier()
+                        .margin(topBottom = 8.px)
+                )
 
                 Row(
-                    Modifier
-                        .fillMaxWidth()
-                        .margin(top = 8.px),
+                    modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.Bottom,
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
@@ -152,16 +121,15 @@ fun PortfolioSection(
                             .padding(right = if (breakpoint > Breakpoint.MD) 100.px else 25.px)
                     ) {
                         selectedWork?.tags?.forEach {
-                            P(
-                                Modifier
+                            SpanText(
+                                text = it.name,
+                                modifier = Modifier
                                     .padding(topBottom = 4.px, leftRight = 6.px)
-                                    .margin(topBottom = 0.px)
                                     .backgroundColor(ColorMode.current.toColorScheme.secondaryContainer)
                                     .color(ColorMode.current.toColorScheme.onSecondaryContainer)
                                     .fontSize(11.px)
                                     .borderRadius(6.px)
-                                    .toAttrs()
-                            ) { Text(it.name) }
+                            )
                         }
                     }
 
@@ -189,16 +157,55 @@ fun PortfolioSection(
     }
 }
 
+val ProjectDescriptionStyle = CssStyle {
+    base {
+        Modifier
+            .fillMaxWidth(100.percent)
+            .margin(top = 24.px, bottom = 0.px)
+            .fontSize(18.px)
+            .color(colorMode.toColorScheme.onPrimary)
+    }
+
+    Breakpoint.MD {
+        Modifier
+            .fillMaxWidth(65.percent)
+            .margin(top = 100.px, bottom = 0.px)
+            .fontSize(24.px)
+    }
+
+    Breakpoint.LG {
+        Modifier.fontSize(36.px)
+    }
+}
+
 val ProjectNameStyle = CssStyle {
+    base {
+        Modifier
+            .color(colorMode.toColorScheme.onPrimary)
+            .padding(topBottom = 4.px, leftRight = 10.px)
+            .margin(topBottom = 0.px)
+            .fontSize(14.px)
+            .borderRadius(6.px)
+            .cursor(Cursor.Pointer)
+            .userSelect(UserSelect.None) // No selecting text within buttons
+            .maxLines(1)
+    }
+
+    cssRule(".active") {
+        Modifier
+            .backgroundColor(colorMode.toColorScheme.surface)
+            .color(colorMode.toColorScheme.onSurface)
+    }
+
     hover {
-        Modifier.backgroundColor(colorMode.toColorScheme.surface.toRgb().copy(alpha = 50))
+        Modifier
+            .color(colorMode.toColorScheme.onPrimary)
+            .backgroundColor(colorMode.toColorScheme.surface.toRgb().copy(alpha = 50))
     }
 
     focus {
-        Modifier.backgroundColor(colorMode.toColorScheme.surface.toRgb().copy(alpha = 50))
-    }
-
-    active {
-        Modifier.backgroundColor(colorMode.toColorScheme.surface.toRgb().copy(alpha = 100))
+        Modifier
+            .color(colorMode.toColorScheme.onPrimary)
+            .backgroundColor(colorMode.toColorScheme.surface.toRgb().copy(alpha = 50))
     }
 }
